ab20d7191c55dae09f8dac123d8bbc1aea9c6ae8
RichtarikOliver <116460423+RichtarikOliver@users.noreply.github.com>
2025-01-31 17:32:18 +0100

    Create dislogin.sh

---
+++ dislogin.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Získa zoznam všetkých bežiacich LXC kontajnerov v Proxmoxe
+containers=$(pct list | awk 'NR>1 {print $1}')
+
+# Nový obsah pre login.json
+new_json='{"login_required": false}'
+
+# Prejde všetky bežiace kontajnery
+echo "Updating login.json in all running LXC containers..."
+for container in $containers; do
+    echo "Updating container: $container"
+    pct exec $container -- bash -c "cd etcmc && echo '$new_json' > login.json"
+done
+
+
+echo "Update complete."
